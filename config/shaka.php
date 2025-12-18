cffafa2941fa7927f762717d76db3a1d09bf1e82
francoism90 <francoism90@users.noreply.github.com>
2025-12-18 20:49:35 +0100

    Add config

--- config/shaka.php
+++ config/shaka.php
@@ -1,6 +1,19 @@
 <?php
 
-// config for Foxws/Shaka
+declare(strict_types=1);
+
 return [
+
+    'packager' => [
+        'binaries' => env('PACKAGER_PATH', '/usr/local/bin/packager'),
+    ],
+
+    'timeout' => 60 * 60 * 4, // 4 hours
+
+    'log_channel' => env('PACKAGER_PATH_LOG_CHANNEL', false),
+
+    'temporary_files_root' => env('PACKAGER_TEMPORARY_FILES_ROOT', storage_path('app/packager/temp')),
+
+    'temporary_files_encrypted_hls' => env('PACKAGER_PATH_TEMPORARY_ENCRYPTED_HLS', '/dev/shm'),
 
 ];
--- src/ShakaServiceProvider.php
+++ src/ShakaServiceProvider.php
@@ -2,23 +2,17 @@
 
 namespace Foxws\Shaka;
 
+use Foxws\Shaka\Commands\ShakaCommand;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use Foxws\Shaka\Commands\ShakaCommand;
 
 class ShakaServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-shaka')
             ->hasConfigFile()
-            ->hasViews()
             ->hasMigration('create_laravel_shaka_table')
             ->hasCommand(ShakaCommand::class);
     }

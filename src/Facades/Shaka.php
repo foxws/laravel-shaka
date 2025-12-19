97403f6fbc729098ea1a2677b7f9f2238ca448e0
francoism90 <francoism90@users.noreply.github.com>
2025-12-19 08:44:07 +0000

    Add strict types declaration and implement ForwardsCalls trait in Shaka class

--- src/Facades/Shaka.php
+++ src/Facades/Shaka.php
@@ -1,10 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Foxws\Shaka\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
 /**
+ * @method static \Foxws\Shaka\Shaka fromDisk($disk)
+ * @method static \Foxws\Shaka\Shaka fromFilesystem(\Illuminate\Contracts\Filesystem\Filesystem $filesystem)
+ * @method static \Foxws\Shaka\Shaka open($path)
+ * @method static \Foxws\Shaka\Shaka cleanupTemporaryFiles()
+ *
  * @see \Foxws\Shaka\Shaka
  */
 class Shaka extends Facade
--- src/Shaka.php
+++ src/Shaka.php
@@ -1,5 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Foxws\Shaka;
 
-class Shaka {}
+use Illuminate\Support\Traits\ForwardsCalls;
+
+class Shaka
+{
+    use ForwardsCalls;
+
+    protected ?string $defaultDisk = null;
+}

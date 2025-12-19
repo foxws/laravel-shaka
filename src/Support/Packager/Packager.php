e87712e729623280a91bdbd09adfcbd776e94a8d
francoism90 <francoism90@users.noreply.github.com>
2025-12-19 09:43:13 +0000

    Implement Packager class with media collection handling methods

--- src/Support/Packager/Packager.php
+++ src/Support/Packager/Packager.php
@@ -4,7 +4,35 @@ declare(strict_types=1);
 
 namespace Foxws\Shaka\Support\Packager;
 
+use Foxws\Shaka\Support\Filesystem\MediaCollection;
+use Illuminate\Support\Traits\ForwardsCalls;
+
 class Packager
 {
-    //
+    use ForwardsCalls;
+
+    protected ?MediaCollection $mediaCollection = null;
+
+    public function fresh(): self
+    {
+        return new static();
+    }
+
+    public function getMediaCollection(): MediaCollection
+    {
+        return $this->mediaCollection;
+    }
+
+    public function open(MediaCollection $mediaCollection): self
+    {
+        $this->mediaCollection = $mediaCollection;
+
+        if ($mediaCollection->count() === 1) {
+            // TODO: process single media mode
+        } else {
+            // TODO: process multiple media mode
+        }
+
+        return $this;
+    }
 }
